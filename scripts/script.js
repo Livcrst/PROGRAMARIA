b892f070c7b3b44dbf528e48c9a169e70f1f145c
Livia Nascimento <livcristinna@gmail.com>
2020-09-22 20:53:42 -0300

    Finish construção de front

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log("Hello Wolrd!")
+document.getElementById('buttonSend').addEventListener('click', () => {
+    if(document.getElementById('name').value  != "" && document.getElementById('email').value != "") {
+        alert("Prontinho! você receberá as novidades por e-mail")
+    
+    }
+    else{
+        alert('Preencha todos os campos!')
+    
+    }
+});
+
+
